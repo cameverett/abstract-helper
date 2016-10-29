8e7b3f0ed1d2c0b2fadcafaf3bb09f363a57e7af
cameverett <cameronjeverett@gmail.com>
2016-10-29 03:28:06 -0400

    resulting permutation from a string of moves represented by characters.

---
+++ example/Rubik/StringOfMoves/stringOfMoves.cpp
@@ -0,0 +1,81 @@
+#include "../../../abstract_algebra.hpp"
+
+using namespace Permutation;
+
+int main()
+{
+    auto cube_moves = *readPermutations( "../rubik.in");
+
+    std::cout << "------------------" << std::endl;
+
+    // Upper case => clockwise U
+    // Lower case => counter-clockwise e.g. U'
+    const std::string F2R("URurufUF");
+
+    composition seriesOfMoves;
+    for( auto &c : F2R)
+    {
+        switch(c)
+        {
+            case('U'):
+                seriesOfMoves.push_front( cube_moves.at("U"));
+                break;
+            case('u'):
+                seriesOfMoves.push_front( *inverse( cube_moves.at("U")));
+                break;
+            case('D'):
+                seriesOfMoves.push_front( cube_moves.at("D"));
+                break;
+            case('d'):
+                seriesOfMoves.push_front( *inverse( cube_moves.at("D")));
+                break;
+            case('L'):
+                seriesOfMoves.push_front( cube_moves.at("L"));
+                break;
+            case('l'):
+                seriesOfMoves.push_front( *inverse( cube_moves.at("L")));
+                break;
+            case('R'):
+                seriesOfMoves.push_front( cube_moves.at("R"));
+                break;
+            case('r'):
+                seriesOfMoves.push_front( *inverse( cube_moves.at("R")));
+                break;
+            case('F'):
+                seriesOfMoves.push_front( cube_moves.at("F"));
+                break;
+            case('f'):
+                seriesOfMoves.push_front( *inverse( cube_moves.at("F")));
+                break;
+            case('B'):
+                seriesOfMoves.push_front( cube_moves.at("B"));
+                break;
+            case('b'):
+                seriesOfMoves.push_front( *inverse( cube_moves.at("B")));
+                break;
+            default:
+                std::cout << "Ignoring invalid move " << c << std::endl;
+                break;
+        }
+    }
+
+
+    std::cout << F2R << " Done as a composition of functions" << std::endl;
+    printCOF( seriesOfMoves);
+
+    std::cout << "\nResult: " << std::endl;
+    auto done = *compose( seriesOfMoves);
+    printCycleNotation( done);
+    std::cout << "\nOrder of the permutation is " << order( done) << std::endl;
+
+    seriesOfMoves.clear();
+    for( int i = 0; i < order(done); i++)
+    {
+        seriesOfMoves.push_front( done);
+    }
+
+    printCycleNotation( *compose( seriesOfMoves));
+
+    std::cout << std::endl;
+    return 0;
+}
